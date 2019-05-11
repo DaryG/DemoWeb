e69087fdb7163ed72fb5ba5212630e6acb822292
dgonzace <dgonzace@LIM-6RVTHM2.usersad.everis.int>
2019-05-10 19:44:38 -0500

    Checkstyles

--- DemoWeb/src/main/java/com/dary/SecurityConfig.java
+++ DemoWeb/src/main/java/com/dary/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dary;
 
+import com.dary.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,40 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.dary.service.UserService;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-	private UserService userDetailsService;
-	@Autowired
-	private BCryptPasswordEncoder bcrypt;
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
-		
-					}
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-	throws Exception{
-		auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-			.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic();
-		
-	}
-	
-	
+  @Autowired
+  private UserService userDetailsService;
+  @Autowired
+  private BCryptPasswordEncoder bcrypt;
+
+  /**Encriptador.
+   * 
+   * @return encripta la clave.
+   */
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bcryptPasswordEncoder;
+
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+
+  }
+
 }
--- DemoWeb/src/main/java/com/dary/controller/DemoController.java
+++ DemoWeb/src/main/java/com/dary/controller/DemoController.java
@@ -1,39 +1,51 @@
 package com.dary.controller;
 
+import com.dary.model.Persona;
+import com.dary.repo.IPersonaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.dary.model.Persona;
-import com.dary.repo.IPersonaRepo;
+
 
 @Controller
 
 public class DemoController {
-	
-	@Autowired
-	private IPersonaRepo repo;
-	 
-	 @GetMapping("/greeting")
-	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		 //logica
-		 Persona p= new Persona();
-		 p.setIdPersona(2);
-		 p.setNombre("Raul1");
-		 repo.save(p);
-	        model.addAttribute("name", name);
-	        return "greeting";
-	    }
-	 
-	 @GetMapping("/listar")
-	    public String greeting( Model model) {
-		 //logica
-		 
-		 
-	        model.addAttribute("personas", repo.findAll());
-	        return "greeting";
-	    }
+
+  @Autowired
+  private IPersonaRepo repo;
+
+  /**persona.
+   * 
+   * @param name id.
+   * @param model nombre.
+   * @return insertar persona.
+   */
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+      Model model) {
+    // logica
+    Persona p = new Persona();
+    p.setIdPersona(2);
+    p.setNombre("Raul");
+    repo.save(p);
+    model.addAttribute("name", name);
+    return "greeting";
+  }
+  
+/**
+ * 
+ * @param model
+ * @return
+ */
+  @GetMapping("/listar")
+  public String greeting(Model model) {
+    // logica
+
+    model.addAttribute("personas", repo.findAll());
+    return "greeting";
+  }
 
 }
